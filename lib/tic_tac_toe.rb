4a9c9845281e1535494ddff86974ed5570990a0c
Danny Brandt <dvbrandt90@gmail.com>
2021-01-06 22:05:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -124,7 +124,7 @@ def won?
     
     tokens = @board.values_at(space1,space2,space3)
     
-    count = tokens.count {|space| space == current_player}
+    count = tokens.count {|space| space == "x" || tokens.count {|space| space == "O" }
     if count == 3 
       return combo.join(",")
     end
